c6bf54dc657ced339739bac6f7ec6e9e1ad40811
Ethan McKinney <ethan.mckinney23@gmail.com>
2024-12-19 17:01:29 -0600

    started to implement weapon data from valorant API, learning their json format, i.e. where is the weapon data, display name found. Organizing weapon data to send correct data to each respective page.

--- valscrape.py
+++ valscrape.py
@@ -11,47 +11,70 @@
 # print(data['data'][2]['weaponStats']['fireRate'])
 
 
-
-# @app.route('/')
-# def hello():
-#     return 'Hello World!'
-
 from flask import Flask, redirect, url_for, request, render_template
+import requests
 app = Flask(__name__)
 
 
+def get_weapons():
+    url = "https://valorant-api.com/v1/weapons"
+    response = requests.get(url)
+    return response.json()
+
+def weapon_list():
+    weapon_data = get_weapons()
+    if weapon_data:
+        weapons = weapon_data['data']
+    else:
+        weapons = []
+    return weapons
+
+weapons = weapon_list()
+# Weapon item attributes:
+# [0] = uuid
+# [1] = displayName
+# [2] = category
+# [3] = defaultSkinUuid
+# [4] = displayIcon
+# [5] = killStreamIcon
+# [6] = assetPath
+# [7] = weaponStats
+# [8] = shopData
+# [9] = skins
+
+# Routes to each page, Pistols, Knives, etc.
 @app.route('/')
 def home():
-    return render_template('basic.html')
+    return render_template('basic.html', weapons=weapons)
 
 # knives, pistols, shotguns, subs, ar, lmg, snipers
 @app.route('/knives.html')
 def knives():
-    return render_template('knives.html')
+    return render_template('knives.html', weapons=weapons)
 
 @app.route('/pistols.html')
 def pistols():
-    return render_template('pistols.html')
+    return render_template('pistols.html', weapons=weapons)
 
 @app.route('/shotguns.html')
 def shotguns():
-    return render_template('shotguns.html')
+    return render_template('shotguns.html', weapons=weapons)
 
 @app.route('/subs.html')
 def subs():
-    return render_template('subs.html')
+    return render_template('subs.html', weapons=weapons)
 
 @app.route('/ar.html')
 def ar(): 
-    return render_template('ar.html')
+    return render_template('ar.html', weapons=weapons)
 
 @app.route('/lmg.html')
 def lmg():
-    return render_template('lmg.html')
+    return render_template('lmg.html', weapons=weapons)
 
 @app.route('/snipers.html')
 def snipers():
-    return render_template('snipers.html')
+    return render_template('snipers.html', weapons=weapons)
 
 
 if __name__ == '__main__':
